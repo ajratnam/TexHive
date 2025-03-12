642e855e1c15228e7b9263a12796755c4ec923c2
Joy2225 <archishman9892@gmail.com>
2025-03-12 14:26:04 +0530

    Exposed port to all interfaces

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from core.extensions import socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
